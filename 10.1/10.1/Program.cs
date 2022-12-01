e616f07304be77d4d68b3dfbacf9920cc48167cb
Yanas76 <116164567+Yanas76@users.noreply.github.com>
2022-12-02 00:13:06 +0200

    Add files via upload

---
+++ 10.1/10.1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Порушено принцип єдиності обов'язку. Клас Order виконує забагато роботи. Можна виправити
+//це створивши нові класи і передавши їм частину обов'язків
+
+class Item
+{
+
+}
+class Order
+{
+    private List<Item> itemList;
+    internal List<Item> ItemList
+    {
+        get
+        {
+            return itemList;
+        }
+        set
+        {
+            itemList = value;
+        }
+    }   
+   
+}
+
+class OrderInformation
+{
+    public void CalculateTotalSum() {/*...*/}
+    public void GetItems() {/*...*/}
+    public void GetItemCount() {/*...*/}
+    public void PrintOrder() {/*...*/}
+    public void ShowOrder() {/*...*/}
+}
+
+class ModifyOrder
+{
+    public void AddItem(Item item) {/*...*/}
+    public void DeleteItem(Item item) {/*...*/}
+    public void Load() {/*...*/}
+    public void Save() {/*...*/}
+    public void Update() {/*...*/}
+    public void Delete() {/*...*/}
+}
